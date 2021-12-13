1130fbb38c762f437ff226da2f642367faa3fa04
Ginpin21 <iftekharahmed2003@gmail.com>
2021-12-13 17:07:43 +0400

    added try and catch block

--- EZCalc.py
+++ EZCalc.py
@@ -116,23 +116,27 @@ def student_menu(u_name):
         7.Back to menu
 =========================================================================
         """)
-        ch=int(input("Enter your choice: "))
-        if ch==1:
-            pass
-        elif ch==2:
-            pass        
-        elif ch==3:
-            pass
-        elif ch==4:
-            pass
-        elif ch==5:
-            pass
-        elif ch==6:
-            change_pass(name1)
-        elif ch==7:
-            break
-        else:
-            print("Invalid choice")
+        try:
+            ch=int(input("Enter your choice: "))
+            if ch==1:
+                pass
+            elif ch==2:
+                pass        
+            elif ch==3:
+                pass
+            elif ch==4:
+                pass
+            elif ch==5:
+                pass
+            elif ch==6:
+                change_pass(name1)
+            elif ch==7:
+                break
+            else:
+                print("Invalid choice")
+                next()
+        except ValueError:
+            print("Please enter an integer")
             next()
 #===============================================================================================================================================
 def teacher_menu(u_name):
@@ -150,32 +154,35 @@ def teacher_menu(u_name):
         6.Back to menu
 =========================================================================
         """)
-        ch=int(input("Enter your choice: "))
-        if ch==1:
-            col_names=["Username","Password","Type"]
-            df=pd.DataFrame() 
-            df=pd.read_csv("Users.csv",names=col_names,index_col="Username")
-            df.sort_values("Type",inplace=True)
-            print(df[df["Type"]=="Student"])
-            next()
-        elif ch==2:
-            a=input("Enter the function name: ")
-            enable(a)
-        elif ch==3:
-            a=input("Enter the function name: ")
-            disable(a)
-        elif ch==4:
-            df2=pd.read_csv("Functions.csv",names=["Function","Enabled"],index_col="Function")
-            print(df2)
-            next()
-        elif ch==5:
-            change_pass(name1)
-        elif ch==6:
-            next()
-            break
-        else:
-            print("Invalid choice")
-            
+        try:
+            ch=int(input("Enter your choice: "))
+            if ch==1:
+                col_names=["Username","Password","Type"]
+                df=pd.DataFrame() 
+                df=pd.read_csv("Users.csv",names=col_names,index_col="Username")
+                df.sort_values("Type",inplace=True)
+                print(df[df["Type"]=="Student"])
+                next()
+            elif ch==2:
+                a=input("Enter the function name: ")
+                enable(a)
+            elif ch==3:
+                a=input("Enter the function name: ")
+                disable(a)
+            elif ch==4:
+                df2=pd.read_csv("Functions.csv",names=["Function","Enabled"],index_col="Function")
+                print(df2)
+                next()
+            elif ch==5:
+                change_pass(name1)
+            elif ch==6:
+                next()
+                break
+            else:
+                print("Invalid choice")
+        except ValueError:
+                print("Please enter an integer")
+                next()        
 #===============================================================================================================================================
 def admin_menu():
     while True:
@@ -188,22 +195,26 @@ def admin_menu():
         3.View all users info
         4.Back to menu
 =========================================================================""")
-        ch=int(input("Enter your choice: "))
-        if ch==1:
-            new_user("Student")
-        elif ch==2:
-            new_user("Teacher")
-        elif ch==3:
-            col_names=["Username","Password","Type"]
-            df=pd.DataFrame()
-            df=pd.read_csv("Users.csv",index_col="Username",names=col_names)
-            print(df)
-            next()
-        elif ch==4:
-            next()
-            break
-        else:
-            print("Invalid choice")
+        try:
+            ch=int(input("Enter your choice: "))
+            if ch==1:
+                new_user("Student")
+            elif ch==2:
+                new_user("Teacher")
+            elif ch==3:
+                col_names=["Username","Password","Type"]
+                df=pd.DataFrame()
+                df=pd.read_csv("Users.csv",index_col="Username",names=col_names)
+                print(df)
+                next()
+            elif ch==4:
+                next()
+                break
+            else:
+                print("Invalid choice")
+                next()
+        except ValueError:
+            print("Please enter an integer")
             next()
 
 #===============================================================================================================================================
@@ -218,48 +229,52 @@ def main_menu():
         3.Login as Admin
         4.Exit
 =========================================================================""")
-        ch=int(input("Enter your choice: "))
-        if ch==1:
-            name1=str(input("Enter the username: "))
-            pass1=str(input("Enter the password: "))
-            col_names=["Username","Password","Type"]
-            df=pd.DataFrame()
-            df=pd.read_csv("Users.csv",index_col="Username",names=col_names)
-            df.sort_values("Type",inplace=True)
-            for i in df.itertuples():
-                if name1==i[0] and pass1==i[1]:             
-                    student_menu(name1)
-                    break
+        try:
+            ch=int(input("Enter your choice: "))
+            if ch==1:
+                name1=str(input("Enter the username: "))
+                pass1=str(input("Enter the password: "))
+                col_names=["Username","Password","Type"]
+                df=pd.DataFrame()
+                df=pd.read_csv("Users.csv",index_col="Username",names=col_names)
+                df.sort_values("Type",inplace=True)
+                for i in df.itertuples():
+                    if name1==i[0] and pass1==i[1]:             
+                        student_menu(name1)
+                        break
+                else:
+                    print("Invalid username or password")
+                    next()
+            elif ch==2:
+                name1=str(input("Enter the username: "))
+                pass1=str(input("Enter the password: "))
+                col_names=["Username","Password","Type"]
+                df=pd.DataFrame()
+                df=pd.read_csv("Users.csv",index_col="Username",names=col_names)
+                df.sort_values("Type",inplace=True)
+                for i in df.itertuples():
+                    if name1==i[0] and pass1==i[1]:             
+                        teacher_menu(name1)
+                        break
+                else:
+                    print("Invalid username or password")
+                    next()
+            elif ch==3:
+                name1=str(input("Enter the username: "))
+                pass1=str(input("Enter the password: "))
+                if name1=="admin" and pass1=="admin123":
+                    admin_menu()
+                else:
+                    print("Invalid username or password")
+                    next()
+            elif ch==4:
+                SystemExit()
+                break
             else:
-                print("Invalid username or password")
+                print("\nInvalid Choice")
                 next()
-        elif ch==2:
-            name1=str(input("Enter the username: "))
-            pass1=str(input("Enter the password: "))
-            col_names=["Username","Password","Type"]
-            df=pd.DataFrame()
-            df=pd.read_csv("Users.csv",index_col="Username",names=col_names)
-            df.sort_values("Type",inplace=True)
-            for i in df.itertuples():
-                if name1==i[0] and pass1==i[1]:             
-                    teacher_menu(name1)
-                    break
-            else:
-                print("Invalid username or password")
-                next()
-        elif ch==3:
-            name1=str(input("Enter the username: "))
-            pass1=str(input("Enter the password: "))
-            if name1=="admin" and pass1=="admin123":
-                admin_menu()
-            else:
-                print("Invalid username or password")
-                next()
-        elif ch==4:
-            SystemExit()
-            break
-        else:
-            print("Invalid Choice")
+        except ValueError:
+            print("Please enter an integer")
             next()
 #===============================================================================================================================================
 main_menu()
